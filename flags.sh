ac66089a1fdb940ef4ceb6df95647ef4fbc14bc7
user5145 <user5145@user.local.lan>
2019-02-26 00:32:45 +0100

    fix flags

--- flags.sh
+++ flags.sh
@@ -1,22 +1,18 @@
 #!/bin/sh
 #
-# you may want to change -mtune= and -march= to generic
-# FLAGS_TEMP+=" -march=native      -mtune=native"
-  FLAGS_TEMP+=" -march=generic     -mtune=native"
-# FLAGS_TEMP+=" -march=native      -mtune=generic"
-# FLAGS_TEMP+=" -march=generic     -mtune=generic"
+# you may want to change -march= to x86-64 and -mtune= to generic
+# FLAGS+=" -march=native           -mtune=native"
+# FLAGS+=" -march=native           -mtune=generic"
+  FLAGS+=" -march=x86-64           -mtune=native"
+# FLAGS+=" -march=x86-64           -mtune=generic"
 #
 #
 # instructions supported by everything from this century (bulldozer(Steamroller)+, Sandy Bridge+)
-# these good ones are not included -mxsavec -mxsaves -mavx2 -msse2avx
 # these may slow down performance -ftree-vectorize -ftree-slp-vectorize
-# FLAGS_TEMP+=" -O2 -pipe -fstack-protector-strong -fno-plt -mmmx -msse -msse2 -mssse3 -msse3 -msse4.1 -msse4.2 -mfpmath=sse -mfma -mf16c -mpclmul -mpopcnt -mlzcnt -mavx -maes -mbmi -mbmi2 -mxsave -mxsaveopt -frecord-gcc-switches -D_FORTIFY_SOURCE=1"
-#
-# these instructions are for everything usable
-FLAGS_TEMP+=" -O2 -pipe -fstack-protector-strong -fno-plt -mmmx -msse -msse2 -mssse3 -msse3 -msse4.1 -msse4.2 -mfpmath=sse -mfma -mf16c -mpclmul -mpopcnt -mlzcnt -mavx -mavx2 -maes -mbmi -mbmi2 -mxsave -mxsavec -mxsaveopt -mxsaves -frecord-gcc-switches -D_FORTIFY_SOURCE=1"
+  FLAGS+=" -O2 -pipe -fstack-protector-strong -fno-plt -mmmx -msse -msse2 -mssse3 -msse3 -msse4.1 -msse4.2 -mfpmath=sse -mfma -mf16c -mpclmul -mpopcnt -mlzcnt -mavx -maes -mbmi -mbmi2 -mxsave -mxsaveopt -frecord-gcc-switches -D_FORTIFY_SOURCE=1"
 # 
 # This is for fun
-# FLAGS_TEMP+=" -O2 -pipe -fstack-protector-strong -fno-plt -frecord-gcc-switches -D_FORTIFY_SOURCE=1"
+  FLAGS+=" -mxsavec -mxsaves -mavx2"
 #
 #
 # to check flags
